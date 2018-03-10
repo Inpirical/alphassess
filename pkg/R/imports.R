0f3d5aef4c8468700b283b05bcb264b372dc9641
jtynes <johannes@inpirical.com>
2018-03-10 13:45:43 -0300

    adding imports file for importFrom infix operators of magrittr package

---
+++ pkg/R/imports.R
@@ -0,0 +1,37 @@
+# Import infix operators from `magrittr` package ===============================
+# See `magrittr` package for documentation.
+
+#' magrittr pipe infix operator
+#'
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @rdname pipe
+#' @export
+NULL
+
+
+#' magrittr exposition pipe operator
+#'
+#' @importFrom magrittr %$%
+#' @name %$%
+#' @rdname pipe_exposition
+#' @export
+NULL
+
+
+#' magrittr pipe assignment compound infix operator
+#'
+#' @importFrom magrittr %<>%
+#' @name %<>%
+#' @rdname pipe_assignment
+#' @export
+NULL
+
+
+#' magrittr pipe 'tee' infix operator
+#'
+#' @importFrom magrittr %T>%
+#' @name %T>%
+#' @rdname pipe_tee
+#' @export
+NULL
